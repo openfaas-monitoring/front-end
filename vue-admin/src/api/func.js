8361ff6350ba741b8bf692d1547c9d8e10a388dc
I527839 <tongyao.xue@sap.com>
2022-08-22 21:56:44 +0800

    add search

--- vue-admin/src/api/func.js
+++ vue-admin/src/api/func.js
@@ -1,8 +1,22 @@
 import request from "./axiosConfig";
 // 获取所有的pods列表
+export const getFuncList = (param) =>{
+	return request({
+		url:'/global/functions',
+		method:'get',
+		param
+	})
+}
 export const getGlobalNodes = (param) => {
 	return request({
 		url:'/global/nodes',
+		method:'get',
+		param
+	})
+}
+export const getLog =(param) =>{
+	return request({
+		url:'/logs',
 		method:'get',
 		param
 	})
